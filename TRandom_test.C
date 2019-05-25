5f25e4f37dc45d8874da9559374c0c0c3ef8c3f4
Ron Belmont <belmonrj@gmail.com>
2019-05-24 20:46:57 -0400

    Minor formatting changes

--- TRandom_test.C
+++ TRandom_test.C
@@ -7,7 +7,8 @@ using namespace std;
 int TRandom_Test()
 {
   srand(time(0));
-  for(int x = 1; x<25;x++){
-    cout << 1+(rand()%6) << endl;
-  }
-} 
+  for(int x = 1; x<25;x++)
+    {
+      cout << 1+(rand()%6) << endl;
+    }
+}
