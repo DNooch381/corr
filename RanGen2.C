567b1426c3949aa6882d7b1fd1bbf387a1ae7f74
Dylan Antonacci <dcantona@uncg.edu>
2019-06-11 12:39:54 -0400

    Comment and formatting organization

--- RanGen2.C
+++ RanGen2.C
@@ -1,51 +1,36 @@
 double get_corr(int);
 
-
-
 void RanGen2()
 {
-  double x = get_corr (9); //number of correlations for the phi1 we want to return
+  double x = get_corr (9); // number of correlations for phi1
   cout <<endl;
   cout <<endl;
-  double y = get_corr (9); //number of correlations for the phi2 we want to return
-}
+  double y = get_corr (9); // number of correlations for phi2
+} //end of void RanGen2()
 
 double get_corr(int npairs)
 {
   TRandom3 angle(0);
-
-  //Dylan TComplex based on shared code
 
   static const int maxCorrelator = 12;
   static const int maxHarmonic = 10;
   static const int maxPower = 9;
   TComplex Qvector[maxHarmonic][maxPower];
 
-  // initialize the Q-vectors to zero
   for(int h=0;h<maxHarmonic;h++)
     {
       for(int w=0;w<maxPower;w++)
         {
-          Qvector[h][w] = TComplex(0.,0.);
-        }
-    }
+          Qvector[h][w] = TComplex(0.,0.); // initialize Q-vector to zero
+        } // end of loop over Qvector
+    } // end of loop over maxHarmonic
 
-  // Receiving some error messages (see below), but think this may be a bracketing issue?
-  // errors being found:
-  //  In file included from input_line_10:1:
-  //  /Users/home_dylanantonacci/Corr/RanGen2.C:36:36: error: use of undeclared identifier 'p'
-  //   Qvector[h][w] += TComplex(cos(h*p.phi()),sin(h*p.phi()));
-  // ^ ---- AH HA!
-  //   /Users/home_dylanantonacci/Corr/RanGen2.C:36:51: error: use of undeclared identifier 'p'
-  //  Qvector[h][w] += TComplex(cos(h*p.phi()),sin(h*p.phi()));
+  vector <double> ang; // inserting pairs into single object
 
-  //vector < double > ang; //inserting the pairs into a single object
-  vector <double> ang; //inserting the pairs into a single object
-
-  // Below is our "for" loop
-  // int = integer, i is the variable that can always be at 0,
+  // int = integer, i can always be at 0,
   // i < 100 means go up to 100 events, ++i checks if it's less
   // than 100 and adds 1 -- if more than 100, than the program stops
+
   for ( int i = 0; i < npairs; ++i )
     {
       //double means an exact number, phi1 is the name of the double for particle 1.
@@ -56,10 +41,11 @@ double get_corr(int npairs)
       cout << phi1 << " " << phi2 << endl;
       cout << i << " " << endl;
 
-      ang.push_back(phi1); //push_back means to stack numbers
+      ang.push_back(phi1); //push_back to stack numbers on return
       ang.push_back(phi2);
 
-    }
+    } // end of npairs for loop
+
   for ( int i = 0; i < ang.size(); ++i )
     {
       cout << ang[i] << " " ;
@@ -69,11 +55,11 @@ double get_corr(int npairs)
           // do the summation for the Q-vectors
           for(int w=0;w<maxPower;w++)
             {
-              //Qvector[h][w] += TComplex(cos(h*p.phi()),sin(h*p.phi()));
               Qvector[h][w] += TComplex(cos(h*phi),sin(h*phi));
             } // end of loop over powers
         } // end of loop over harmonics
     } // end of loop over ang vector
+
   cout <<endl;
   return 0.0;
 }
