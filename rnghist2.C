958479afe1edfe2e092c0db9b681168c19814577
Dylan Antonacci <dcantona@uncg.edu>
2019-06-18 14:00:43 -0400

    histo fit code added

--- rnghist2.C
+++ rnghist2.C
@@ -6,6 +6,9 @@ void rnghist2()
   TCanvas* c1 = new TCanvas("c1","",800,600);
   TF1* fun = new TF1("fun","[0]/(x-1)",1.9, 500);
 
+  histogram->Fit(fun,"R");
+  c1->Print("histogram_fit_500logylogx.png");
+
   histogram->GetXaxis()->SetRangeUser(0,20);
   histogram->Draw();
   c1->Print("histogram_20.png");
