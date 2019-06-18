e41f35f85b265bbeec7860733358f29a878e10c6
Dylan Antonacci <dcantona@uncg.edu>
2019-06-18 12:55:58 -0400

    RanGen2.C copy/paste

--- rnghist2.C
+++ rnghist2.C
@@ -1,5 +1,12 @@
 //new file for Histogram
 
+#include "Recursion.C"
+#include <sys/time.h>
+
+void get_corr(int);
+
+void do_recursion(vector<double>&); // start of recursion function
+   
 void rnghist2()
 {
-}
+} 
