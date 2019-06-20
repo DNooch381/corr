40f5f564202f1f90d7b2cbf82036554d2214b675
Dylan Antonacci <dcantona@uncg.edu>
2019-06-20 14:30:29 -0400

    testing fits

--- rnghist2.C
+++ rnghist2.C
@@ -3,11 +3,6 @@ void rnghist2()
 
   TFile* HistFile = new TFile("FileTwo.root","read");
   TProfile* histogram = (TProfile*)HistFile->Get("hmult_recursion_0_0");
-<<<<<<< HEAD
-  TCanvas* c1 = new TCanvas("c1","",800,600);
-  TF1* fun = new TF1("fun","[0]/(x-1)",1.9, 500); //includes combinatoric function for 2 particle correlations
-=======
->>>>>>> 3c60bb78ecfe34db4b8ce978dd32872b2cf08cb6
 
   TCanvas* c1 = new TCanvas("c1","",800,600);
 
@@ -15,25 +10,24 @@ void rnghist2()
   histogram->GetXaxis()->SetTitle("Number of particles");
   histogram->GetXaxis()->SetRangeUser(0,20);
   histogram->Draw();
-  c1->Print("histogram_20.png");
+  c1->Print("histogram_20_4.png");
 
   histogram->GetXaxis()->SetRangeUser(0,500);
   histogram->Draw();
-  c1->Print("histogram_500.png");
+  c1->Print("histogram_500_4.png");
 
   c1->SetLogy();
   histogram->Draw();
-  c1->Print("histogram_500logy.png");
+  c1->Print("histogram_500logy_4.png");
 
   c1->SetLogx();
   histogram->Draw();
-  c1->Print("histogram_500logylogx.png");
+  c1->Print("histogram_500logylogx_4.png");
 
   // --- correct combinatoric function
   TF1* fun = new TF1("fun","[0]/(x-1)",1.9, 500);
 
   histogram->Fit(fun,"R");
-  c1->Print("histogram_fit_500logylogx.png");
+  c1->Print("histogram_fit_500logylogx_4.png");
 
 } //end of rnghist2
-
