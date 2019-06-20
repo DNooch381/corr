7e127fad9aa8c68bf56e4287da6c248cf6ea2e51
Dylan Antonacci <dcantona@uncg.edu>
2019-06-20 14:42:22 -0400

    redrawn plots

--- rnghist2.C
+++ rnghist2.C
@@ -10,24 +10,24 @@ void rnghist2()
   histogram->GetXaxis()->SetTitle("Number of particles");
   histogram->GetXaxis()->SetRangeUser(0,20);
   histogram->Draw();
-  c1->Print("histogram_20_4.png");
+  c1->Print("histogram_20_10.png");
 
   histogram->GetXaxis()->SetRangeUser(0,500);
   histogram->Draw();
-  c1->Print("histogram_500_4.png");
+  c1->Print("histogram_500_10.png");
 
   c1->SetLogy();
   histogram->Draw();
-  c1->Print("histogram_500logy_4.png");
+  c1->Print("histogram_500logy_10.png");
 
   c1->SetLogx();
   histogram->Draw();
-  c1->Print("histogram_500logylogx_4.png");
+  c1->Print("histogram_500logylogx_10.png");
 
   // --- correct combinatoric function
   TF1* fun = new TF1("fun","[0]/(x-1)",1.9, 500);
 
   histogram->Fit(fun,"R");
-  c1->Print("histogram_fit_500logylogx_4.png");
+  c1->Print("histogram_fit_500logylogx_10.png");
 
 } //end of rnghist2
