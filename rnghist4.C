13a8d6a1e3740237fec77f76380a4a611b801f3a
Ron Belmont <belmonrj@gmail.com>
2019-06-20 18:27:29 -0400

    New directory for figures, small update to rnghist2, new macro rnghist4

--- rnghist4.C
+++ rnghist4.C
@@ -1,7 +1,7 @@
-void rnghist2()
+void rnghist4()
 {
 
-  TFile* HistFile = new TFile("FileTwo.root","read");
+  TFile* HistFile = new TFile("FileFour.root","read");
   TProfile* histogram = (TProfile*)HistFile->Get("hmult_recursion_0_0");
 
   TCanvas* c1 = new TCanvas("c1","",800,600);
@@ -10,19 +10,19 @@ void rnghist2()
   histogram->GetXaxis()->SetTitle("Number of particles");
   histogram->GetXaxis()->SetRangeUser(0,20);
   histogram->Draw();
-  c1->Print("Figures/histogram2_20.png");
+  c1->Print("Figures/histogram4_20.png");
 
   histogram->GetXaxis()->SetRangeUser(0,500);
   histogram->Draw();
-  c1->Print("Figures/histogram2_500.png");
+  c1->Print("Figures/histogram4_500.png");
 
   c1->SetLogy(1);
   histogram->Draw();
-  c1->Print("Figures/histogram2_500logy.png");
+  c1->Print("Figures/histogram4_500logy.png");
 
   c1->SetLogx(1);
   histogram->Draw();
-  c1->Print("Figures/histogram2_500logylogx.png");
+  c1->Print("Figures/histogram4_500logylogx.png");
 
   // --- correct combinatoric function
   TF1* fun = new TF1("fun","[0]/(x-1)",1.9, 500);
@@ -30,18 +30,18 @@ void rnghist2()
   // --- redraw the same histograms above but with the fit
 
   histogram->Fit(fun,"R");
-  c1->Print("Figures/histogram2_fit_500logylogx.png");
+  c1->Print("Figures/histogram4_fit_500logylogx.png");
 
   c1->SetLogx(0);
   histogram->Draw();
-  c1->Print("Figures/histogram2_fit_500logy.png");
+  c1->Print("Figures/histogram4_fit_500logy.png");
 
   c1->SetLogy(0);
   histogram->Draw();
-  c1->Print("Figures/histogram2_fit_500.png");
+  c1->Print("Figures/histogram4_fit_500.png");
 
   histogram->GetXaxis()->SetRangeUser(0,20);
   histogram->Draw();
-  c1->Print("Figures/histogram2_fit_20.png");
+  c1->Print("Figures/histogram4_fit_20.png");
 
 } //end of rnghist2
