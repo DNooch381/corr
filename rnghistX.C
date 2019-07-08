1489fe4c292bbaf152134b3eff95c6c89513390b
Ron Belmont <belmonrj@gmail.com>
2019-07-08 10:38:31 -0400

    This almost works fine, but need to work on a better file naming scheme

---
+++ rnghistX.C
@@ -0,0 +1,64 @@
+void plotfit(TProfile*,TF1*,const char*);
+
+void rnghistX()
+{
+
+  TFile* HistFile2 = new TFile("OutputFiles/OutFile_k2.root","read");
+  TProfile* histogram2 = (TProfile*)HistFile2->Get("hmult_recursion_0_0");
+  TF1* fun2 = new TF1("fun2","[0]/(x-1)",1.9, 500);
+  char handle2[20] = "2";
+  plotfit(histogram2,fun2,handle2);
+
+  TFile* HistFile4 = new TFile("OutputFiles/OutFile_k4.root","read");
+  TProfile* histogram4 = (TProfile*)HistFile4->Get("hmult_recursion_0_0");
+  char handle4[40] = "4";
+  plotfit(histogram4,fun2,handle4);
+
+  TProfile* histogram44 = (TProfile*)HistFile4->Get("hmult_recursion_0_2");
+  TF1* fun4 = new TF1("fun4","[0]/((x-1)*(x-2)*(x-3))",3.9, 500);
+  char handle44[40] = "44";
+  plotfit(histogram44,fun4,handle44);
+
+}
+
+void plotfit(TProfile* histogram, TF1* fun, const char* handle)
+{
+
+  TCanvas* c1 = new TCanvas("c1","",800,600);
+
+  histogram->GetYaxis()->SetTitle("c_{2}{2}");
+  histogram->GetXaxis()->SetTitle("Number of particles");
+  histogram->GetXaxis()->SetRangeUser(0,20);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_20.png",handle));
+
+  histogram->GetXaxis()->SetRangeUser(0,500);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_500.png",handle));
+
+  c1->SetLogy(1);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_500logy.png",handle));
+
+  c1->SetLogx(1);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_500logylogx.png",handle));
+
+  // --- redraw the same histograms above but with the fit
+
+  histogram->Fit(fun,"R");
+  c1->Print(Form("Figures/histogram%s_fit_500logylogx.png",handle));
+
+  c1->SetLogx(0);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_fit_500logy.png",handle));
+
+  c1->SetLogy(0);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_fit_500.png",handle));
+
+  histogram->GetXaxis()->SetRangeUser(0,20);
+  histogram->Draw();
+  c1->Print(Form("Figures/histogram%s_fit_20.png",handle));
+
+} //end of rnghist2
