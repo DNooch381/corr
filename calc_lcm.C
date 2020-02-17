2b9b97df86de9aab19aead3e57a05619b05bc92f
Ron Belmont <belmonrj@gmail.com>
2020-02-17 16:29:39 -0500

    Nice little improvement here

--- calc_lcm.C
+++ calc_lcm.C
@@ -13,19 +13,12 @@ int get_lcm(int num1, int num2)
 
   cout << "For " << num1 << " and " << num2 << ", ";
 
-   int maxValue = (num1 > num2) ? num1 : num2;
+  int maxValue = (num1 > num2) ? num1 : num2;
 
-   while(1)
-   {
-      //condition of LCM
-      if((maxValue % num1 == 0) && (maxValue % num2 == 0))
-      {
-         cout << "the LCM is " << maxValue << endl;
-         break;
-      }
-      ++maxValue;
-   }
-   return maxValue;
+  while ( ((maxValue % num1 == 0) && (maxValue % num2 == 0)) == false ) ++maxValue;
+
+  cout << "the LCM is " << maxValue << endl;
+
+  return maxValue;
 
 }
-
