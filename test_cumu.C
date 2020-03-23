1979e64a8e6d0ee3ecfc3c3a2f4580873c43431d
Ron Belmont <belmonrj@gmail.com>
2020-03-23 15:03:16 -0400

    Hmm, this is pretty interesting

--- test_cumu.C
+++ test_cumu.C
@@ -3,7 +3,8 @@ void test_cumu()
 
   TCanvas* c1 = new TCanvas("c1","");
 
-  TFile* file = TFile::Open("CondorOutput/SumFile_k2.root");
+  //TFile* file = TFile::Open("CondorOutput/SumFile_k2.root"); // doesn't work, maybe it shouldn't
+  TFile* file = TFile::Open("CondorOutput/SumFile_k4.root"); // hmm
 
   TProfile* tp1f_two = (TProfile*)file->Get("hmult_recursion_0_0");
   TProfile* tp1f_four = (TProfile*)file->Get("hmult_recursion_0_2");
@@ -35,6 +36,7 @@ void test_cumu()
   c1->Print("TestFigs/test_fig_222and4.png"); // looks fine
 
   th1d_two->Add(th1d_four,-1.0);
+  th1d_two->Scale(-1);
 
   th1d_two->Draw();
   c1->SetLogy();
@@ -44,7 +46,7 @@ void test_cumu()
 
   for ( int i = 0; i < tp1f_two->GetNbinsX(); ++i )
     {
-      if ( i%2 == 1 ) continue;
+      if ( i%4 != 0 ) continue;
       double two = tp1f_two->GetBinContent(i+1);
       double four = tp1f_four->GetBinContent(i+1);
       double twotwotwo = 2*two*two;
@@ -56,13 +58,14 @@ void test_cumu()
     }
 
   hc4->Draw();
+  hc4->GetXaxis()->SetRangeUser(0,500);
   c1->SetLogy();
   c1->Print("TestFigs/test_fig_cleaned_c4.png");
 
   TF1* fun = new TF1("fun","[0]/pow(x,[1])",0,500);
-  fun->FixParameter(0,10.0);
-  fun->FixParameter(1,3.0);
-  hc4->Fit(fun,"","",4,100);
+  fun->FixParameter(0,0.5);
+  fun->FixParameter(1,2.0);
+  hc4->Fit(fun,"","",4,500);
   c1->SetLogy();
   c1->Print("TestFigs/test_fig_fitcleaned_c4.png");
 
