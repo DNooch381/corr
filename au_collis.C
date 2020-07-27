aeb42c759ab7a81fc120e79331561a59be3d43bf
Dylan Antonacci <dcantona@uncg.edu>
2020-07-27 15:49:26 -0400

    added some comments and general spacing changes

--- au_collis.C
+++ au_collis.C
@@ -2,33 +2,43 @@
 void au_collis()
 {
 
-  //TFile* f2 = TFile::Open("histos_12432.root");
-  TFile* f2 = TFile::Open("histos_13799.root");
+  // --- "Get" file(s)
+  
+  //TFile* f2 = TFile::Open("OutputFiles/RootFiles/histos_12432.root");
+  TFile* f2 = TFile::Open("OutputFiles/RootFiles/histos_13799.root");
+
+  // --- "Get" histogram(s) from file(s)
+  
   TProfile* tp1f_2 = (TProfile*)f2->Get("nfvtxt_recursion_0_0");
 
+  // --- Axis, Plots, Scaling, and Formatting Code  
+  
   TCanvas* c1 = new TCanvas("c1","");
 
   tp1f_2->SetMarkerStyle(kFullCircle);
   tp1f_2->SetMarkerColor(kBlack);
   tp1f_2->Draw("ex0p");
-  // suggest to start with looking at low numbers
+
+  // Note: suggest to start with looking at low "SetRangeUser numbers
+
   tp1f_2->GetXaxis()->SetRangeUser(0,100);
   tp1f_2->GetXaxis()->SetTitle("Pending");
   tp1f_2->GetYaxis()->SetTitle("Pending");
 
   c1->SetLogy();
   c1->SetLogx();
+
   //c1->Print("Figures/au_collis.png");
 
   TF1* fun1 = new TF1("fun1","[0]/pow(x,1)",1.0,99.9);
   fun1->SetParameter(0,1e-2); // asymptotic behavior from combinatorics (0!)
+
   TF1* fun2 = new TF1("fun2","[0]/((x-1))",1.0,99.9);
   fun2->SetParameter(0,1e-2); // combinatorics suggests 0!
   fun2->SetLineColor(kBlue);
 
   fun1->Draw("same");
   fun2->Draw("same");
-
 
   // Legend
 
@@ -37,7 +47,8 @@ void au_collis()
   leg->AddEntry(fun1,"power law function","l");
   leg->Draw();
 
-  // this name needs to match
+  // Note: this name needs to match
+
   c1->Print("Figures/au_collis_100logxlogy.png");
   c1->SetLogx(0);
 
