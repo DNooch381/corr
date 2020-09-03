de77dae2dd7a2403e40a4a03e6a1f34e72cbb1fc
Dylan Antonacci <dcantona@uncg.edu>
2020-09-03 13:39:07 -0400

    added comment towards bottomf or missing elements

--- d_au200GeV3.C
+++ d_au200GeV3.C
@@ -13,7 +13,6 @@ void d_au200GeV3()
   //string line;
 
   // --- Open file
-
   double x[nbins]; // number of tracks in the PHENIX FVTX
   double y[nbins]; // v2{2} (two-particle cumulant v2)
   double ey[nbins]; // statistical uncertainty
@@ -27,10 +26,15 @@ void d_au200GeV3()
     }
 
   TGraphErrors* tge_v22 = new TGraphErrors(nbins,x,y,0,ey);
+  
   tge_v22->SetMarkerStyle(kFullSquare);
   tge_v22->SetMarkerColor(kRed);
+  tge_v22->SetLineColor(kBlack);
+  
   tge_v22->Draw("ap");
 
+  // To be added: best fit line, print graph to TFile, adjust graph axis, add graph key, add axis class names
+  
   return 0;
 
 }
